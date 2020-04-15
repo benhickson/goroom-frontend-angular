4febb641aaba3d41a872e725ae8e0b4471cd448e
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-15 17:07:04 -0400

    ci: build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.goroom.benhickson.com',
-  whitelistedDomains: ['https://api.goroom.benhickson.com'],
+  whitelistedDomains: ['api.goroom.benhickson.com'],
   agora: {
     appId: '516c8452e59e4505aed64d368917ffca'
   }
